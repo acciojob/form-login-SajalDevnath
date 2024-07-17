d1eddab114baa3c7358001c93bdd127b3887d358
SajalDevnath <63283752+SajalDevnath@users.noreply.github.com>
2024-07-17 17:15:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
-    //Write your code here
+    const form = document.querySelector('form');
 
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const firstName = document.querySelector('input[name="first_name"]').value;
+        const lastName = document.querySelector('input[name="last_name"]').value;
+        
+        alert(`${firstName} ${lastName}`);
+    });
 }
+
